9665d77589d196d7574784dadd471a7be156a2b4
nivinabbas <nivin.abbas1@gmail.com>
2021-12-06 00:00:23 +0200

    add city schema

---
+++ server/model/City.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/expense');
+mongoose.connect('mongodb://localhost/city', { useNewUrlParser: true });
+
+const Schema = mongoose.Schema;
+
+const citySchema = new Schema({
+  name: String,
+  temperature: Number,
+  condition: String,
+  conditionPic: String,
+});
+
+const City = mongoose.model('city', citySchema);
+
+module.exports = City;
