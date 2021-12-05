5bbc99792e43c3cc51a37104e732a34c4d638fce
nivinabbas <nivin.abbas1@gmail.com>
2021-12-06 00:00:10 +0200

    add server listener

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const api = require('./server/routes/api');
+const express = require('express');
+const app = express();
+let port = 5500;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'model')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use('/', api);
+
+app.listen(port, function (request, response) {
+  console.log(`Server is up`);
+});
