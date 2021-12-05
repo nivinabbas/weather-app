7d03a14a77a414f19d93059bd78ade649e783f01
nivinabbas <nivin.abbas1@gmail.com>
2021-12-06 00:00:40 +0200

    add api routes server

---
+++ server/routes/api.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+// const moment = require('moment');
+const mongoose = require('mongoose');
+const City = require('../model/City');
+const urllib = require('urllib');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/expense');
+
+mongoose.connect('mongodb://localhost/city', { useNewUrlParser: true });
+
+let API_KEY = 'b35017ebf137421ec453d05614795211';
+
+let city;
+router.get('/city/:cityName', function (request, response) {
+  let cityName = request.params.cityName;
+  urllib.request(
+    `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`,
+    function (req, res) {
+      let cityData = JSON.parse(res);
+      city = new City({
+        name: cityName,
+        temperature: cityData.main.temp,
+        condition: cityData.weather[0].main,
+        conditionIcon: `http://openweathermap.org/img/wn/${cityData.weather[0].icon}@2x.png`,
+      });
+
+      response.send(city);
+    }
+  );
+});
+
+router.get('/cities', function (request, response) {
+  City.find({}, function (err, cities) {
+    response.send(cities);
+  });
+});
+
+router.post('/city', function (request, res) {
+  let addCity = new City({
+    name: request.body.name,
+    temperature: request.body.temperature,
+    condition: request.body.condition,
+    conditionIcon: request.body.conditionIcon,
+  });
+  addCity.save().then((city) => {
+    console.log(`The name of city is : ${city.name} ${city.temperature}`);
+  });
+  res.send(addCity);
+});
+
+router.delete('/city/:cityName', function (request, response) {
+  let cityName = request.params.cityName;
+  City.findOneAndDelete({ name: cityName }, function (err, city) {
+    console.log(city);
+  });
+  response.end();
+});
+
+module.exports = router;
